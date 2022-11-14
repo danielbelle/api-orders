d9b440d5e909e0d4e9dec63f11d13d878dbac611
Daniel Henrique Bellé <47600437+danielbelle@users.noreply.github.com>
2022-11-14 14:52:45 -0300

    DES-012 - Finalizado

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,35 +8,8 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('token');
+        $orders_detail='';
+        return view('list_order',array($orders_detail));
     }
 
-    public function getKey()
-    {
-        return "QWERTYASDFGH";
-    }
-
-    public function login()
-    {
-        $iat = time();
-        $nbf = $iat + 5;  // não pode usar o token antes de
-        $exp = $iat + 600; // token expira
-
-        $payload = [
-            "iat" => $iat,
-            "nbf" => $nbf,
-            "exp" => $exp,
-            "userdata" => 'informações do usuário'
-        ];
-
-        $token = JWT::encode($payload, $this->getKey(), 'HS256');
-
-        header('Token', $token);
-
-        $authorization = apache_request_headers();
-        $res = $this->request->getServer('HTTP_AUTHORIZATION');
-        $response_http = service('response');
-
-        return view('login');
-    }
 }
